fefa7b0762ac74c87477c89cb795fc10cdd1ae6e
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2023-10-05 20:52:43 -0300

    Commit

--- Barber-Shop-App/App.js
+++ Barber-Shop-App/App.js
@@ -8,9 +8,9 @@ const Stack = createStackNavigator();
 function MyStackNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="HomeClient" component={HomeClient}/>
-      <Stack.Screen name="HomeBarber" component={HomeBarber}/>
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="HomeClient" component={HomeClient} options={{ headerShown: false }}/>
+      <Stack.Screen name="HomeBarber" component={HomeBarber} options={{ headerShown: false }}/>
 
     </Stack.Navigator>
   );
